81967afe8884b56b9a4f1a7f29bec2b9cf920626
SirDoggyJvla <106160594+SirDoggyJvla@users.noreply.github.com>
2024-06-03 01:11:59 +0200

    update

--- Contents/mods/Zomboid Forge/media/lua/client/ZomboidForge.lua
+++ Contents/mods/Zomboid Forge/media/lua/client/ZomboidForge.lua
@@ -47,54 +47,7 @@ end
 ZomboidForge.OnGameStart = function()
     -- Zomboid (base game zombies)
 	if SandboxVars.ZomboidForge.ZomboidSpawn then
-		ZomboidForge.ZTypes.ZF_Zomboid = {
-            -- base informations
-            name = "IGUI_ZF_Zomboid",
-            chance = SandboxVars.ZomboidForge.ZomboidChance,
-            --outfit = {},
-            --reanimatedPlayer = false,
-            --skeleton = false,
-			-- hair = {
-			-- 	male = {
-			-- 		"",
-			-- 	},
-			-- 	female = {
-			-- 		"",
-			-- 	},
-			-- },
-			-- hairColor = {
-			-- 	ImmutableColor.new(Color.new(0.70, 0.70, 0.70, 1)),
-			-- },
-			-- beard = {
-			-- 	"",
-			-- },
-            --animationVariable = nil,
-
-            -- stats
-            walktype = SandboxVars.ZomboidForge.ZomboidWalktype,
-            strength = SandboxVars.ZomboidForge.ZomboidStrength,
-            toughness = SandboxVars.ZomboidForge.ZomboidToughness,
-            cognition = SandboxVars.ZomboidForge.ZomboidCognition,
-            memory = SandboxVars.ZomboidForge.ZomboidMemory,
-            sight = SandboxVars.ZomboidForge.ZomboidVision,
-            hearing = SandboxVars.ZomboidForge.ZomboidHearing,
-
-            --noteeth = false,
-
-            -- UI
-            color = {255, 255, 255,},
-            outline = {0, 0, 0,},
-
-			-- attack functions
-			-- zombieAgro = {},
-			-- zombieOnHit = {},
-
-			-- custom behavior
-			-- zombieDeath = {},
-			-- customBehavior = {},
-
-			-- customData = {},
-        }
+        ZomboidForge.AddZomboids()
 	end
 end
 
--- Contents/mods/Zomboid Forge/media/lua/client/ZomboidForge_tools.lua
+++ Contents/mods/Zomboid Forge/media/lua/client/ZomboidForge_tools.lua
@@ -27,6 +27,58 @@ ZomboidForge.initModData_ZomboidForge_tools = function()
     ZFModData = ModData.getOrCreate("ZomboidForge")
 end
 
+-- Modders can overwrite this to stop Zomboids from being ever activated by other players
+ZomboidForge.AddZomboids = function()
+    ZomboidForge.ZTypes.ZF_Zomboid = {
+        -- base informations
+        name = "IGUI_ZF_Zomboid",
+        chance = SandboxVars.ZomboidForge.ZomboidChance,
+        --outfit = {},
+        --reanimatedPlayer = false,
+        --skeleton = false,
+        -- hair = {
+        -- 	male = {
+        -- 		"",
+        -- 	},
+        -- 	female = {
+        -- 		"",
+        -- 	},
+        -- },
+        -- hairColor = {
+        -- 	ImmutableColor.new(Color.new(0.70, 0.70, 0.70, 1)),
+        -- },
+        -- beard = {
+        -- 	"",
+        -- },
+        --animationVariable = nil,
+
+        -- stats
+        walktype = SandboxVars.ZomboidForge.ZomboidWalktype,
+        strength = SandboxVars.ZomboidForge.ZomboidStrength,
+        toughness = SandboxVars.ZomboidForge.ZomboidToughness,
+        cognition = SandboxVars.ZomboidForge.ZomboidCognition,
+        memory = SandboxVars.ZomboidForge.ZomboidMemory,
+        sight = SandboxVars.ZomboidForge.ZomboidVision,
+        hearing = SandboxVars.ZomboidForge.ZomboidHearing,
+
+        --noteeth = false,
+
+        -- UI
+        color = {255, 255, 255,},
+        outline = {0, 0, 0,},
+
+        -- attack functions
+        -- zombieAgro = {},
+        -- zombieOnHit = {},
+
+        -- custom behavior
+        -- zombieDeath = {},
+        -- customBehavior = {},
+
+        -- customData = {},
+    }
+end
+
 local A1, A2 = 727595, 798405  -- 5^17=D20*A1+A2
 local D20, D40 = 1048576, 1099511627776  -- 2^20, 2^40
 -- Seeded random used in determining the `ZType` of a zombie.
